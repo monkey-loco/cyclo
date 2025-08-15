ac51d5d09eb46c90698694e28ccbad01fd691088
Marco <20408709+marco-paulucci@users.noreply.github.com>
2025-08-15 19:11:46 +0200

    dev build

---
+++ dev/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Marco Paulucci-Cyclo-0.0.2";
+const contentToCache = [
+    "Build/Cyclo Releases.loader.js",
+    "Build/Cyclo Releases.framework.js.unityweb",
+    "Build/Cyclo Releases.data.unityweb",
+    "Build/Cyclo Releases.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
